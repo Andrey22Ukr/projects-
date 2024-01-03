cc9c7fbbcac12552ba1eaccddac153c42cf560d6
andto <and.tolstikoff@gmail.com>
2024-01-03 19:32:20 +0100

    commit1

---
+++ HW2.py
@@ -0,0 +1,11 @@
+num1 = int(input("enter number1:"))
+num2 = int(input("enter number2:"))
+num3 = int(input("enter number3:"))
+print("num1=",num1,"num2=",num2,"num3=",num3)
+result = num1 + num2 + num3
+print(f"Your result is {result}")
+
+diagonal1 = int(input("enter diagonal1:"))
+diagonal2 = int(input("enter diagonal2"))
+result = (diagonal1 * diagonal2) / 2
+print(result)
